f3307ca4ea9e2cc65a8dae0d5bc88efb08be2b52
root <jlbrmn@gmail.com>
2019-10-09 22:12:10 +0000

    setup adjustments

--- files/setup.sh
+++ files/setup.sh
@@ -4,9 +4,9 @@ cd /opt
 echo '---
 - hosts: server
   roles:
-     - ans-model
-   environment:
-     STRING_ONE: "{{str_one}}"
-     STRING_TWO: "{{str_two}}"' > mmodal_playbook.yml
+    - ans-mmodal
+  environment:
+    STRING_ONE: "{{str_one}}"
+    STRING_TWO: "{{str_two}}"' > mmodal_playbook.yml
 
 echo 'localhost' > inventory
