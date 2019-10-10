7d1d3648e13d1be16b06c13766e3b35d3e66bdb3
AI-Joe <jlbrmn@gmail.com>
2019-10-09 21:02:23 -0400

    whether they both are anagrams

--- files/anagram.py
+++ files/anagram.py
@@ -1,6 +1,6 @@
 import os
 
-if ''.join(reversed(os.getenv('STRING_ONE'))) == os.getenv('STRING_TWO'):
+if ''.join(reversed(os.getenv('STRING_ONE'))) == os.getenv('STRING_ONE') and ''.join(reversed(os.getenv('STRING_TWO'))) == os.getenv('STRING_TWO'):
     print(True)
 else:
     print(False)
