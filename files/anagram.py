4660e9dc88eae4c83bc4c04398d4dfcc5fa4290b
AI-Joe <jlbrmn@gmail.com>
2019-10-09 21:19:12 -0400

    anagram sort not reverse

--- files/anagram.py
+++ files/anagram.py
@@ -1,7 +1,10 @@
 import os
 
-if ''.join(reversed(os.getenv('STRING_ONE'))) == os.getenv('STRING_ONE') and ''.join(reversed(os.getenv('STRING_TWO'))) == os.getenv('STRING_TWO'):
-    print(True)
+if os.getenv('STRING_ONE').isalpha() and os.getenv('STRING_TWO').isalpha():
+    if sorted(os.getenv('STRING_ONE')) == sorted(os.getenv('STRING_TWO')):
+        print(True)
+    else:
+        print(False)
 else:
     print(False)
 
